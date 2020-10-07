369de261ae47b8a581f71b6aaad6bce6bf2f3626
Kah Wei <msntkw@msn.com>
2020-10-07 13:19:35 +0800

    Added back the / to the front of the comment chunk so that it is valid

--- out/extension.js
+++ out/extension.js
@@ -24,11 +24,11 @@ function activate(context) {
         provideCompletionItems(document, position, token, context) {
             // File Comment Header Auto Completion
             const fileCommentCompletion = new vscode.CompletionItem('Documentation: File Level');
-            fileCommentCompletion.insertText = new vscode.SnippetString('*!\n@file       @todo\n@author     ' + authorStr + '\n@course     @todo\n@section    ' + section + '\n@assignment @todo\n@date       ' + currentDate + '\n@brief      @todo\n\n*//*__________________________________________________________________________*/\n\n');
+            fileCommentCompletion.insertText = new vscode.SnippetString('/*!\n@file       @todo\n@author     ' + authorStr + '\n@course     @todo\n@section    ' + section + '\n@assignment @todo\n@date       ' + currentDate + '\n@brief      @todo\n\n*//*__________________________________________________________________________*/\n\n');
             fileCommentCompletion.documentation = new vscode.MarkdownString("Inserts the file level documentation header.");
             // Function Comment Header Auto Completion
             const functionCommentCompletion = new vscode.CompletionItem('Documentation: Function Level');
-            functionCommentCompletion.insertText = new vscode.SnippetString('*!\n@brief  @todo\n\n@param  @todo\n@return @todo\n*//*__________________________________________________________________________*/');
+            functionCommentCompletion.insertText = new vscode.SnippetString('/*!\n@brief  @todo\n\n@param  @todo\n@return @todo\n*//*__________________________________________________________________________*/');
             functionCommentCompletion.documentation = new vscode.MarkdownString("Inserts the function level documentation header.");
             // return all completion items as array
             return [
